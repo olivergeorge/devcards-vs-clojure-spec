ce99941e9baaf9c733c6f306c3fe263a2eb29613
Oliver George <oliver@condense.com.au>
2016-09-26 23:03:35 +1000

    Use lorem-ipsum generator for more interesting/realistic example

--- src/devcards_vs_clojure_spec/example.cljs
+++ src/devcards_vs_clojure_spec/example.cljs
@@ -4,6 +4,7 @@
   (:require
     [cljs.spec :as s]
     [devcards-vs-clojure-spec.core :as core]
+    [devcards-vs-clojure-spec.generators :as gen]
     [sablono.core :as sab]))
 
 (defn my-table
@@ -18,12 +19,14 @@
                      [:td (get row k "-")])])]]))
 
 (s/def ::col-key keyword?)
+(s/def ::cell-val string?)
 
 (s/fdef my-table
   :args (s/cat :keys (s/? (s/coll-of ::col-key))
-               :rows (s/coll-of (s/map-of ::col-key string?))))
+               :rows (s/coll-of (s/map-of ::col-key ::cell-val))))
 
 (core/add-override ::col-key #(s/gen #{:A :B :C}))
+(core/add-override ::cell-val (gen/build-lorem-ipsum-generator 0 16))
 
 (defcard my-table-with-controls
   (core/controls (core/sampler #'my-table)))
