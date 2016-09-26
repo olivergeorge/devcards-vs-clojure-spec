78f9c2e6555d2793df4f4357d341e159b0c19cbb
Oliver George <oliver@condense.com.au>
2016-09-27 09:03:04 +1000

    Take logic out of the controls view

--- src/devcards_vs_clojure_spec/core.cljs
+++ src/devcards_vs_clojure_spec/core.cljs
@@ -39,36 +39,38 @@
 
 (defn controls-view
   "Pure view control wrapper"
-  [{:keys [sampler seed size overrides change-seed change-size]}]
-  (let [[args ret] (sampler {:seed seed :size size :overrides overrides})]
-    (sab/html
-      [:div
-       [:div.form-inline
-        [:div.form-group
-         [:label "Seed " seed]
-         [:button.btn.btn-sm {:on-click #(change-seed (inc seed))} "+"]
-         [:button.btn.btn-sm {:on-click #(change-seed (dec seed))} "-"]]
-        [:div.form-group
-         [:label "Size " size]
-         [:input.form-control
-          {:type      "range"
-           :min       0
-           :max       100
-           :value     size
-           :on-change #(change-size (.-target.value %))}]]]
-       [:div
-        [:div ret]
-        [:hr]
-        [:div (edn-renderer/html-edn args)]]])))
+  [{:keys [args ret seed size change-seed change-size]}]
+  (sab/html
+    [:div
+     [:div.form-inline
+      [:div.form-group
+       [:label "Seed " seed]
+       [:button.btn.btn-sm {:on-click #(change-seed (inc seed))} "+"]
+       [:button.btn.btn-sm {:on-click #(change-seed (dec seed))} "-"]]
+      [:div.form-group
+       [:label "Size " size]
+       [:input.form-control
+        {:type      "range"
+         :min       0
+         :max       100
+         :value     size
+         :on-change #(change-size (.-target.value %))}]]]
+     [:div
+      [:div ret]
+      [:hr]
+      [:div (edn-renderer/html-edn args)]]]))
 
 (defn controls
   [sampler]
   (fn [app-db]
     (let [{:keys [seed size overrides]} @app-db
           size (or size 10)
-          seed (or seed 1)]
+          seed (or seed 1)
+          overrides (merge @overrides-ref overrides)
+          [args ret] (sampler {:seed seed :size size :overrides overrides})]
       (controls-view
-        {:overrides   (merge @overrides-ref overrides)
+        {:args        args
+         :ret         ret
          :sampler     sampler
          :seed        seed
          :size        size
