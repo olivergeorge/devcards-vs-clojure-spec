61f7d5c6e668be4a05b20a5a4de5dba190638677
Oliver George <oliver@condense.com.au>
2016-09-26 20:19:00 +1000

    fix clojure dep

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/olivergeorge/devcards-vs-clojure-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "clojure-1.9.0-alpha12"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/test.check "0.9.0"]
                  [devcards "0.2.1-7"]])
