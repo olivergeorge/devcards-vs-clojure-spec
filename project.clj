a0cbc645c0cfb512ce69807df95b157b53990d21
Oliver George <oliver@condense.com.au>
2016-09-26 20:12:00 +1000

    basic deps

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject devcards-vs-clojure-spec "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Use interesting generated data on your devcards."
+  :url "https://github.com/olivergeorge/devcards-vs-clojure-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "clojure-1.9.0-alpha12"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/test.check "0.9.0"]
+                 [devcards "0.2.1-7"]])
